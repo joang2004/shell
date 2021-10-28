c1f233800e6d393e3f1613156f4a8a6c4d0f61d3
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-10-28 12:06:08 +0200

    versions bones

--- condicional1.sh
+++ condicional1.sh
@@ -2,10 +2,11 @@ echo "Introdueix dos nombres per comparar-los"
 echo "Introdueix el primer valor: "; read num1
 echo "Introdueix el segon valor: "; read num2
 echo ""
-if [ $num1 -gt $num2]
+if [ $num1 -gt $num2 ]
  then
  echo "$num1 és major que $num2"
 elif [ $num1 -eq $num2 ]
+then
  echo "$num1 és igual que $num2"
 else
  echo "$num1 és menor que $num2"
