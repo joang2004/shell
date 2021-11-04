d5e05a2cdc223dbf108288b1b48b0c57346257e2
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-04 12:04:53 +0100

    Update bucle3.sh

--- bucle3.sh
+++ bucle3.sh
@@ -1,6 +1,9 @@
+# Fes un programa que mostri per pantalla del 1 al 10
+# Joan Garí Parera
+
 nom=1
 while [ $nom -le 10 ]
 do
-echo "El nombre és: $nom"
-let nom=$nom+1
+  echo "El nombre és: $nom"
+  let nom=$nom+1
 done
