b7145f89f6eab1bd936c479f051a7df486ed4573
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-04 12:03:05 +0100

    Update bucle2.sh

--- bucle2.sh
+++ bucle2.sh
@@ -1,6 +1,10 @@
+#Fes un programa que mostri per pantalla els nombres del 10 al 1.
+#Joan Garí Parera
+
 nom=10
+
 while [ $nom -ge 0 ]
 do
-echo "El nombre és: $nom"
-let nom=$nom-1
+  echo "El nombre és: $nom"
+  let nom=$nom-1
 done
