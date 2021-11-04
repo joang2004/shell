7f0cc95e5dfe826973f917b9391f5c82f2c2d2ef
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-04 12:01:43 +0100

    Add files via upload

---
+++ problemabucle.sh
@@ -0,0 +1,16 @@
+#El programa demana un nombre a l’usuari. Desde aquest nombre el programa ha de mostrar els 20 següents positius.
+#Joan Garí Parera
+
+echo "En aquest programa introduirás un nombre i el programa et dirà els 20 següents"
+
+echo "Introdueix un nombre: "; read num
+
+sum=0
+echo "Els següents 20 nombres són:"
+while [ $sum -lt 20 ]
+do
+    let num=$num+1
+   
+    let sum=$sum+1
+    echo $num
+done
