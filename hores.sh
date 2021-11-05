1e95d31870d1f2a134457adf0d9e235e3b9a7d0a
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-05 09:07:29 +0100

    Update hores.sh

--- hores.sh
+++ hores.sh
@@ -1,3 +1,6 @@
+#Càlcul temporal
+#Joan Garí Parera
+
 
 echo "En aquest programa has d'introduir un valor positiu en segons i et dira el valor en hores minuts i segons."; echo "" ;echo "Introdueix un nombre positiu en segons: "; read temps
 hores=0
@@ -5,13 +8,13 @@ min=0
 seg=0
 if [ $temps -ge 60 ]
 then
-let min=$temps/60
-let seg=$temps%60
+  let min=$temps/60
+  let seg=$temps%60
 fi
 if [ $min -ge 60 ]
 then
-let hores=$min/60
-let min=$min%60
+  let hores=$min/60
+  let min=$min%60
 fi
 echo "En $temps segons hi ha $hores hores $min minuts i $seg segons"
 
