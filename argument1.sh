f3db9d73519ac5cfd6da31bea40c0ac5665857e3
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-11 11:39:21 +0100

    Add files via upload

---
+++ argument1.sh
@@ -0,0 +1,6 @@
+#Primer exercici d'arguments. L'usuari introdueix dos nombres mitjançant arguments i el programa fa la suma"
+#Joan Garí Parera
+
+let sum=$1+$2
+
+echo "La suma dels dos nombres és: $sum"
