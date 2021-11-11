135f8faec41f9b03ca82c9811c8c4eebf838004d
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-11 11:30:56 +0100

    Update array2.sh

--- array2.sh
+++ array2.sh
@@ -1,11 +1,27 @@
-paraules=( a h )
+ #En un array, el programa contarà el nombre de h's
+ #Joan Garí Parera
+
+echo "Introdueix caràcters separats per un espai. El programa contarà les h"
+echo "Exemple d'input: h a s H df H h fg h"
+printf "Introdueix els caràcters: "
+read paraules
 conta=0
+
 for var in ${paraules[@]} 
 do
-    if [ $var = h or $var = H]
+    if [ $var = h -o $var = H ]
     then
         let conta=$conta+1
     fi
 
 done 
-echo "Hi ha: $conta h(s)."
+
+if [ $conta -gt 1 ]
+then
+    echo "Hi ha $conta h's"
+elif [ $conta -eq 1 ]
+then
+    echo "Hi ha $conta h"
+else
+    echo "No hi ha cap h"
+fi
