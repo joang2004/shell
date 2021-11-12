86c417e3cd3f471dc377ec4a2214e6b7df35ed7f
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-12 09:48:24 +0100

    Add files via upload

---
+++ horamaxima.sh
@@ -0,0 +1,10 @@
+echo "Introdueix quatre nombres entre 0 i 9 i el programa et dirà l'hora màxima que pot formar"
+printf "Introdueix el primer valor: "; read nombre[0]
+printf "Introdueix el segon valor: "; read nombre[1]
+printf "Introdueix el tercer valor: "; read nombre[2]
+printf "Introdueix el quart valor: "; read nombre[3]
+
+for i in ${nombre[@]}
+do
+    if
+done
