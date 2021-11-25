73ec15186496698f85a3a45cb91ce76857f8249a
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-25 19:48:36 +0100

    Versió Final

--- quadrats.sh
+++ quadrats.sh
@@ -1,16 +1,28 @@
 #Joan Garí Parera
 #Introdueix els valors x's mintjançant arguments han de ser entre 1 i 9.
-#El programa et crearà un quadrat x*x i hanira contant d'un en un, quan arribi a 9 gafarà el valor de 0. En canviar de variable agafarà el valor de 0
+#El programa et crearà un quadrat x*x format pel nombre x
 
 echo "El programa et demanarà un nombre i et imprimira x*x des de 1 al 9 "
 valors=$*
+valid=0 #És una buleana, si val 0 tot s'activarà, si val 1, donarà ERROR
 
-for a in ${valors[@]}
+for a in ${valors[@]} #Control de valors
 do
-        columna=1 #marca la posició de la columna que està escrivint
-    linea=1 #marca el nombre de linees que s'han escrites
-    if [ $a -eq 1 ] || [ $a -eq 2 ] || [ $a -eq 3 ] || [ $a -eq 4 ] || [ $a -eq 5 ] || [ $a -eq 6 ] || [ $a -eq 7 ]|| [ $a -eq 8 ] || [ $a -eq 9 ] #Control de valors
+    if [ $a != "1" ] && [ $a != "2" ] && [ $a != "3" ] && [ $a != "4" ] && [ $a != "5" ] && [ $a != "6" ] && [ $a != "7" ] && [ $a != 8 ] && [ $a != "9" ] #Control de valors
     then
+        valid=1
+    fi
+done
+
+    
+    
+if [ $valid -eq 0 ]
+then
+    for a in ${valors[@]}
+    do
+        columna=1 #marca la posició de la columna que està escrivint
+        linea=1 #marca el nombre de linees que s'han escrites
+   
         while [ $columna -le $a ]
         do
             printf $a
@@ -23,11 +35,12 @@ do
                 linea=1
                 let columna=$columna+1
             fi
-            done
-    else
-        echo "ERROR"
-        echo "Per favor introdueix un valor entre 1 i 9"
-    fi
+        done
+    
 
-echo ""
-done 
+    echo ""
+    done
+else
+    echo "ERROR"
+    echo "Per favor introdueix un valor entre 1 i 9"
+fi 
