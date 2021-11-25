90bf1fb3dab15729f8cf83994c1a06794ac66398
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-25 19:35:56 +0100

    Versió Final

--- quadrats3.sh
+++ quadrats3.sh
@@ -6,12 +6,22 @@
 echo "El programa et demanarà un nombre i et imprimira x*x des de 1 al 9 "
 valors=$*
 nombre=0 #marca el nombre que ha de sortir
-for a in ${valors[@]}
+valid=0 #és una buleana, si val 0 s'activarà el programa, si no, sortirà error
+for a in ${valors[@]} #Control de valors
 do
-    columna=1 #marca la posició de la columna que està escrivint
-    linea=1 #marca el nombre de linees que s'han escrites
-    if [ $a -eq 1 ] || [ $a -eq 2 ] || [ $a -eq 3 ] || [ $a -eq 4 ] || [ $a -eq 5 ] || [ $a -eq 6 ] || [ $a -eq 7 ]|| [ $a -eq 8 ] || [ $a -eq 9 ]
+    if [ $a != "1" ] && [ $a != "2" ] && [ $a != "3" ] && [ $a != "4" ] && [ $a != "5" ] && [ $a != "6" ] && [ $a != "7" ] && [ $a != 8 ] && [ $a != "9" ] #Control de valors
     then
+    valid=1
+    fi
+done
+if [ $valid -eq 0 ]
+then
+    for a in ${valors[@]}
+    do
+        columna=1 #marca la posició de la columna que està escrivint
+        linea=1 #marca el nombre de linees que s'han escrites
+    
+    
         while [ $columna -le $a ]
         do
             if [ $nombre -lt 9 ]
@@ -31,10 +41,10 @@ do
                 let columna=$columna+1
             fi
             done
-    else
-        echo "ERROR"
-        echo "Per favor introdueix un valor entre 1 i 9"
-    fi
 
 echo ""
 done 
+else
+    echo "ERROR"
+    echo "Per favor introdueix un valor entre 1 i 9"
+fi
