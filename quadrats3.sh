917ff5dec82c7143bc21a9c40912470f6bbd760a
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-25 17:34:42 +0100

    Update quadrats3.sh

--- quadrats3.sh
+++ quadrats3.sh
@@ -1,32 +1,40 @@
-echo "El programa et demanarà un nombre i et imprimira x*x des de 0 al 9 "
+#Quadrats.sh
+#Joan Garí Parera
+#Introdueix els valors x's mintjançant arguments han de ser entre 1 i 9.
+#El programa et crearà un quadrat x*x i hanira contant d'un en un, quan arribi a 9 gafarà el valor de 0
+
+echo "El programa et demanarà un nombre i et imprimira x*x des de 1 al 9 "
 valors=$*
 nombre=0 #marca el nombre que ha de sortir
 for a in ${valors[@]}
 do
-columna=1 #marca la posició de la columna que està escrivint
-linea=1 #marca el nombre de linees que s'han escrites
+    columna=1 #marca la posició de la columna que està escrivint
+    linea=1 #marca el nombre de linees que s'han escrites
+    if [ $a -eq 1 ] || [ $a -eq 2 ] || [ $a -eq 3 ] || [ $a -eq 4 ] || [ $a -eq 5 ] || [ $a -eq 6 ] || [ $a -eq 7 ]|| [ $a -eq 8 ] || [ $a -eq 9 ]
+    then
+        while [ $columna -le $a ]
+        do
+            if [ $nombre -lt 9 ]
+            then
+            printf $nombre
+            let nombre=$nombre+1
+            else
+            printf $nombre
+            nombre=0
+            fi
+            if [ $linea -lt $a ]
+            then 
+                let linea=$linea+1
+            else
+                echo ""
+                linea=1
+                let columna=$columna+1
+            fi
+            done
+    else
+        echo "ERROR"
+        echo "Per favor introdueix un valor entre 1 i 9"
+    fi
 
-while [ $columna -le $a ]
-do
-    #if [ $linea -lt $a ]
-    #then
-        if [ $nombre -lt 9 ]
-        then
-        printf $nombre
-        let nombre=$nombre+1
-        else
-        printf $nombre
-        nombre=0
-        fi
-        if [ $linea -lt $a ]
-        then 
-            let linea=$linea+1
-        else
-        echo ""
-        linea=1
-        let columna=$columna+1
-        fi
-done
 echo ""
 done 
-
