5010a95ec16cf8b8eac6ea952a02582b383a393f
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-25 21:17:54 +0100

    Versió Final

--- problemabucle.sh
+++ problemabucle.sh
@@ -1,5 +1,5 @@
 #El programa demana un nombre a l’usuari. Desde aquest nombre el programa ha de mostrar els 20 següents positius.
-#Joan Garí Parera
+#Joan Garí Parera 
 
 echo "En aquest programa introduirás un nombre i el programa et dirà els 20 següents"
 
@@ -10,7 +10,7 @@ echo "Els següents 20 nombres són:"
 while [ $sum -le 20 ]
 do
     let num=$num+1
-   
+
     let sum=$sum+1
     echo $num
 done
