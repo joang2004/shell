12be70d95bda41f614bbf4925d28714d68c3d903
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-26 09:49:31 +0100

    Versió Final

--- exerciciVar2.sh
+++ exerciciVar2.sh
@@ -1,3 +1,8 @@
+
+#Joan Garí Parera
+echo "Aquest programa farà la suma, la resta i la multiplicació de tres nombres introduïdes per l'usuari"
+echo ""
+
 echo "Introdueix el primer valor:"
 read num1
 echo "Introdueix el segon valor:"
@@ -13,5 +18,3 @@ echo "La resta de les tres variables és: $res"
 echo ""
 let res=$num1*$num2*$num3
 echo "La multiplicació de les tres variables és:$res"
-
-
