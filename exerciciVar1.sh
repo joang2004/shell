502171fe6a63deb6407df59922a072a92fbd0558
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-11-26 09:46:20 +0100

    Versió Final

--- exerciciVar1.sh
+++ exerciciVar1.sh
@@ -1,3 +1,5 @@
+echo "Aquest programa farà la suma, la resta i la multiplicació de tres variables perdefinides."
+echo ""
 num1=2
 num2=3
 num3=4
@@ -10,5 +12,3 @@ echo "La resta de les tres variables és: $res"
 echo ""
 let res=$num1*$num2*$num3
 echo "La multiplicació de les tres variables és:$res"
-
-
