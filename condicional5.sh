49d62714daddd6c3e155e239f5ea8aeb4fd2b91c
joang2004 <68154930+joang2004@users.noreply.github.com>
2021-12-05 12:06:01 +0100

    V.F

--- condicional5.sh
+++ condicional5.sh
@@ -5,20 +5,20 @@ ran2=$(($RANDOM%101))
 echo "EXERCICI DE CONDICIONALS"
 echo ""
 echo "Introdueix dos nombres  per comparar-los"
-echo "Introdueix el primer valor: "; read num1
-echo "Introdueix el segon valor: "; read num2
+printf "Introdueix el primer valor: "; read num1
+printf "Introdueix el segon valor: "; read num2
 echo ""
 
 if [ $num1 -gt $num2 ]
 then
-    echo "a)$num1 és major que $num2"
+    echo "a) El més gran és $num1, l'altre és $num2."
 
-elif [ $num1 -eq $num2 ]
+elif [ $num1 -lt $num2 ]
 then
-    echo "a)$num1 és igual que $num2"
+    echo "a) El més gran és $num2, l'altre és $num1."
 
 else
-    echo "a)$num1 és menor que $num2"
+    echo "a) Els dos nombres són iguals."
 fi
 
 let num3=$num1*2
@@ -26,35 +26,34 @@ echo ""
 
 if [ $num3 -gt $num2 ]
 then
-    echo "b)El nombre $num1 multiplicat per dos, que dona $num3, és més gran que $num2"
+    echo "b) El nombre $num1 multiplicat per dos, que dona $num3, és més gran que $num2"
 elif [ $num3 -lt $num2 ]
 then
-    echo "b)El nombre $num1 multiplicat per dos, que dona $num3, és menor que $num2"
+    echo "b) El nombre $num1 multiplicat per dos, que dona $num3, és menor que $num2"
 else
-    echo "b)El nombre $num1 multiplicat per dos, que dona $num3, és igual que $num2"
+    echo "b) El nombre $num1 multiplicat per dos, que dona $num3, és igual que $num2"
 fi
 
 echo ""
 
 if [ $num1 -gt $ran1 ]
 then
-	echo "c)El teu nombre, que és $num1, és més gran que el nombre generat, que és $ran1."
+	echo "c) El teu nombre, que és $num1, és més gran que el nombre generat, que és $ran1."
 elif [ $num1 -lt $ran1 ]
  		then
-		echo "c)El teu nombre, que és $num1, és menor que el nombre generat, que és $ran1."
+		echo "c) El teu nombre, que és $num1, és menor que el nombre generat, que és $ran1."
 	else
- 		echo "c)El teu nombre, que és $num1, és igual que el nombre generat."
+ 		echo "c) El teu nombre, que és $num1, és igual que el nombre generat."
 fi
 echo ""
 
 
 if [ $ran1 -gt $ran2 ]
 then
-	echo "d)El primer nombre generat, que és $ran1, és més gran que el segon, que és $ran2."
+	echo "d) El primer nombre generat, que és $ran1, és més gran que el segon, que és $ran2."
 elif [ $ran1 -lt $ran2 ]
 then
-	echo "d)El primer nombre generat, que és $ran1, és més petit que el segon, que és $ran2."
+	echo "d) El primer nombre generat, que és $ran1, és més petit que el segon, que és $ran2."
 else
-	echo "El primer nombre generat, que és $ran1 és igual que el segon."
+	echo "d) El primer nombre generat, que és $ran1 és igual que el segon."
 fi
-
